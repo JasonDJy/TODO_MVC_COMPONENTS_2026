8228776e40cc4363a4850cd403a4f08a6f8ba215
Delbovier Jason <jasondj2000@outlook.com>
2026-09-22 14:59:18 +0200

    NotCompletedCount

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import TodoList from "./components/todoList/TodoList";
 
 new TodoList({
   el: "#app",
-  title: "My Amazing TodoList",
+  title: "À FAIRE !",
   apiURL: "https://6aa920f72d442cb69d4971f8.mockapi.io/api/endpoint",
 }).render();
